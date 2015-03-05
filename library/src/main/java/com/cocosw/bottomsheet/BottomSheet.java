396eeca1aa6e1b29abe32c84aea6b1ebd2d8902b
soar <soarcn@gmail.com>
2015-03-05 10:42:17 +0800

    update copyright

--- library/src/main/java/com/cocosw/bottomsheet/BottomSheet.java
+++ library/src/main/java/com/cocosw/bottomsheet/BottomSheet.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2011, 2015 Kai Liao
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.cocosw.bottomsheet;
 
 import android.annotation.SuppressLint;
@@ -54,7 +70,7 @@ import java.util.List;
  * https://www.google.com/design/spec/components/bottom-sheets.html
  *
  * Project: BottomSheet
- * Created by LiaoKai(soarcn) on 2014/9/21.
+ * Created by Kai Liao on 2014/9/21.
  */
 public class BottomSheet extends Dialog implements DialogInterface {
 
