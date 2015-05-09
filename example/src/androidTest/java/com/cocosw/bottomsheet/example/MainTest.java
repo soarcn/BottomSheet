1729f4d6e19ca5dc0c778895418274cce9009200
soar <soarcn@gmail.com>
2015-05-09 13:16:46 +1000

    remove test now

--- example/src/androidTest/java/com/cocosw/bottomsheet/example/MainTest.java
+++ example/src/androidTest/java/com/cocosw/bottomsheet/example/MainTest.java
@@ -1,19 +1,6 @@
 package com.cocosw.bottomsheet.example;
 
-import android.support.test.espresso.Espresso;
 import android.test.ActivityInstrumentationTestCase2;
-
-import static android.support.test.espresso.Espresso.onData;
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static android.support.test.espresso.action.ViewActions.typeText;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.instanceOf;
 
 public class MainTest extends ActivityInstrumentationTestCase2<Main> {
 
@@ -28,7 +15,7 @@ public class MainTest extends ActivityInstrumentationTestCase2<Main> {
     }
 
     public void testDisplayed() {
-        onView(withId(R.id.listView)).check(matches(isDisplayed()));
+    //    onView(withId(R.id.listView)).check(matches(isDisplayed()));
     }
 
 
